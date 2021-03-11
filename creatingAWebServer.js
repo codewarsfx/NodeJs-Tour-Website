a55b7170fd0d3d720d3e210be37799a8cda0f6c5
codewarsfx <chiderainnocent001@gmail.com>
2021-03-11 03:43:45 +0100

    routing

--- creatingAWebServer.js
+++ creatingAWebServer.js
@@ -1,4 +1,5 @@
 const http= require('http')
+const url= require('url');
 
 
 //lets create our own web server and
@@ -7,7 +8,19 @@ const http= require('http')
 // first create the server and then start the server 
 
 const server= http.createServer((req, res) => {
-    res.end('hello from the server')
+    const pathName = req.url
+    if(pathName === '/overview' || pathName === '/'){
+        res.end('this is the overview page ')
+    }
+    if(pathName === '/product'){
+        res.end('this is the product page')
+    }
+    else {
+        res.writeHead(404,{
+            contentType:'text/html',
+        })
+        res.end('<h1> error page not found </h1>')
+    }
 })
 
 server.listen(8000,'127.0.0.1',()=>{
