4653adfe00ef9dae7fafc82835feff6059dab6cc
codewarsfx <chiderainnocent001@gmail.com>
2021-03-12 12:29:43 +0100

    serving up html template

--- creatingAWebServer.js
+++ creatingAWebServer.js
@@ -3,7 +3,40 @@ const url= require('url');
 const fs = require('fs');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`,'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/cardTemplate.html`,'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/productTemplate.html`,'utf-8')
+
 const dataObject= JSON.parse(data)
+
+
+
+
+const replaceTemplate = (template, product)=>{
+    
+    let output= template.replace(/{%productName%}/g,product.productName);
+     
+    output= output.replace(/{%price%}/g,product.price);
+    output= output.replace(/{%image%}/g,product.image);
+    
+    output=output.replace(/{%quantity%}/g,product.quantity);
+    
+    output=output.replace(/{%description%}/g,product.description);
+    output=output.replace(/{%id%}/g,product.id);
+    output=output.replace(/{%productFrom%}/g,product.from);
+    output=output.replace(/{%productNutrient%}/g,product.nutrients);
+    
+    if(!product.organic){
+        output=output.replace(/{%organic%}/g,'not-organic')
+    }
+    return output
+    
+}
+
+
+
+
+
 
 
 //lets create our own web server and
@@ -13,18 +46,41 @@ const dataObject= JSON.parse(data)
 
 const server= http.createServer((req, res) => {
     const pathName = req.url
+    //overview page 
     if(pathName === '/overview' || pathName === '/'){
-        res.end('this is the overview page ')
+        
+        res.writeHead(200, {
+            "content-type": "text/html"
+        })
+        
+        
+        const cardsHtml = dataObject.map(item=>(
+            replaceTemplate(tempCard,item))
+        ).join('')
+        
+        
+       const finalServe= tempOverview.replace(/{%product-card%}/g,cardsHtml)
+          
+        res.end(finalServe)
+        
     }
+    
+    //product page
     if(pathName === '/product'){
+
         res.end('this is the product page')
     }
+    
+    //api
     if(req.url === '/api'){
             res.writeHead(200,{
                 "content-type":'application/json',
             })
             res.end(data)
         }
+        
+        
+    //not found
     else {
         res.writeHead(404,{
             contentType:'text/html',
