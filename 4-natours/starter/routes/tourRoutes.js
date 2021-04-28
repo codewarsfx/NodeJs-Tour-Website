03f9f71adcab0db76a8063979e313d0830a4a066
codewarsfx <chiderainnocent001@gmail.com>
2021-04-28 23:57:50 +0100

    chaining middleware

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -3,6 +3,18 @@ const express = require('express')
 
 const tourController= require(`../controllers/tourController`)
 
+
+const checkBody = (req,res,next)=>{
+    const {name,price} = req.body 
+    
+    if(!(name && price )) return res.status(400).json({
+        "status": "bad request",
+        "message " :" incorrect request body"
+    })
+    
+    next()
+}
+
 const Router = express.Router();
 Router.param('id',(req,res,next,val)=>{
     if(val > tourController.data.length ) return res.status(404).json({
@@ -14,7 +26,7 @@ Router.param('id',(req,res,next,val)=>{
 
 
 
-Router.route('/').get(tourController.getTours).post(tourController.createTour)
+Router.route('/').get(tourController.getTours).post(checkBody,tourController.createTour)
 Router.route('/:id').patch(tourController.updateTour).delete(tourController.deleteTour).get(tourController.getTour)
 
 
