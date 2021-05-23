36821c39c5200ef35674364d26cc5b48edfefb7f
codewarsfx <chiderainnocent001@gmail.com>
2021-05-23 22:41:01 +0100

    connecting a mongodb cluster instance using mongoose

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,15 +1,24 @@
 
-const dotenv= require('dotenv')
+ require('dotenv').config({ path: `${__dirname}/../.env`})
 
-dotenv.config({
-    path: './config.env'
+const mongoose = require('mongoose');
+
+
+const db = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
+
+mongoose.connect(db,{
+    useNewUrlParser:true,
+    useFindAndModify:false,
+    useCreateIndex:true
+}).then(() => {
+    console.log('database connected successfully')
 })
 
 const app = require('./app')
 
 //SERVER
-const port =3000
+const port =process.env.PORT ||3000
 app.listen(port,()=>{
-    console.log('app is running on port 3000')
+    console.log(`app is running on port ${port}`) 
 })
 
