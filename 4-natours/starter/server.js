4227af8dc57d111cc92e1972608e0888e2c2d545
codewarsfx <chiderainnocent001@gmail.com>
2021-05-26 13:50:47 +0100

    connected nodejs app to mongodb database hosted on atlas

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,24 +1,59 @@
+require('dotenv').config({ path: `${__dirname}/../.env`})
 
- require('dotenv').config({ path: `${__dirname}/../.env`})
 
 const mongoose = require('mongoose');
+const app = require('./app')
 
 
 const db = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
 
-mongoose.connect(db,{
+mongoose.connect(db, {
     useNewUrlParser:true,
     useFindAndModify:false,
-    useCreateIndex:true
+    useCreateIndex:true,
+    useUnifiedTopology: true 
 }).then(() => {
     console.log('database connected successfully')
 })
 
-const app = require('./app')
+
+
+//Schema blueprint 
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true,"a Name is required "],
+        unique: true   
+    },
+    rating: {
+        type:Number,
+        default:4.5
+    },
+    price: {
+        type:Number,
+        required:[true,"A price value must be presented"]
+    }
+})
+
+const Tour = mongoose.model('Tour',tourSchema)
+
+const newTour = new Tour({
+    name:"chidera",
+    rating: 5.3,
+    price:500
+})
+
+newTour.save().then(doc=>console.log(doc))
+
+
+  
+
 
 //SERVER
-const port =process.env.PORT ||3000
+const port =process.env.PORT || 3000
 app.listen(port,()=>{
     console.log(`app is running on port ${port}`) 
 })
+
 
