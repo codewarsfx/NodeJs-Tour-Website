dd385dcf6a25bdbf8261919503d8f6c0cc442fea
codewarsfx <chiderainnocent001@gmail.com>
2021-07-18 17:35:53 +0100

    app clean up

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,10 +1,47 @@
+const handleDevelopmentErrors = (err,res) => {
+     res.status(err.statusCode).json({
+          err,
+          status: err.status,
+          message: err.message,
+          stack: err.stack
+     })
+}
+
+
+const handleProductionErrors = (err,res) => {
+     if(err.operationalError){
+          
+          res.status(err.statusCode).json({
+               status:err.status,
+               message: err.message,
+          })
+          
+     }
+     else{
+          
+          res.status(500).json({
+               message:"Something went very wrong",
+               status:"fail"
+          })
+          
+     }
+
+}
+
+
+
+//Global handling error middleware
 module.exports = (error,req,res,next) =>{
      error.statusCode = error.statusCode || 500
      error.status = error.status || "error"
      
-     res.status(error.statusCode).json({  
-          "status" : error.status,
-          "message" : error.message
-     })   
+     if(process.env.NODE_ENV === "development"){
+          handleDevelopmentErrors(error,res)
+     }
+     else if(process.env.NODE === "production"){
+          handleProductionErrors(error,res)
+          
+     } 
      next()
 }
+
